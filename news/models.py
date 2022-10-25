ba06274976b0504b96afc92c0dc526066843b7e3
Michel Moraes <michelmsnascimento@gmail.com>
2022-10-25 18:27:33 -0300

    #04 ckeditor. concluido

--- news/models.py
+++ news/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils import timezone
-
+from ckeditor_uploader.fields import RichTextUploadingField
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
-    conteudo = models.TextField()
-    data_publicacao = models.DateTimeField(default=timezone.now())
+    conteudo = RichTextUploadingField()
+    data_publicacao = models.DateTimeField(timezone.now())
 
     def __str__(self):
         return self.titulo
