d57946abeff65c1dc225a93644541e524821e3d1
Michel Moraes <michelmsnascimento@gmail.com>
2022-10-24 23:52:10 -0300

    #02 segundo commit. Primeiro Model ()

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Post(models.Model):
+    autor = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField(default=datetime.now())
