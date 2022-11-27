7a704393658b76a08ec63112264eb77f40cdbaf7
Michel Moraes <michelmsnascimento@gmail.com>
2022-11-27 20:51:21 -0300

    seccond commit

--- news/urls.py
+++ news/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
-  
+    path('posts/<int:post_id>/post_detail', views.post_detail, name="post_detail"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
