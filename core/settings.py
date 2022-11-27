5ddd50f100e1f11cb9b2413ee8838c24f9c150f4
Michel Moraes <michelmsnascimento@gmail.com>
2022-11-27 20:15:15 -0300

    settings

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['protto-esports.herokuapp.com']
+ALLOWED_HOSTS = ['prottoesports.herokuapp.com', '.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
