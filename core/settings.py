0d4b6569e4fbdb10316d857ac361df1709bdeabb
Michel Moraes <michelmsnascimento@gmail.com>
2022-11-29 22:06:36 -0300

    'commitse'

--- core/settings.py
+++ core/settings.py
@@ -167,8 +167,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = str(config('EMAIL_HOST_USER'))
-EMAIL_HOST_PASSWORD = str(config('EMAIL_HOST_PASSWORD'))
+EMAIL_HOST_USER = 'prottotech2022@gmail.com'
+EMAIL_HOST_PASSWORD = 'dxvktsfdpjyzndwa'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
